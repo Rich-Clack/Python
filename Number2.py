06b3b8d71c2b961b54989ee3106c2d45211d335e
Rich-Clack <rclacknbs@gmail.com>
2019-05-24 14:30:19 +0100

    new python file added

---
+++ Number2.py
@@ -0,0 +1,15 @@
+no=int(input("Enter Number :"))
+if no>1000:
+      print("B")
+      if no>2000:
+            print("C")
+      if no<=2000:
+      	    print("F")   
+      print("2")	          
+else:
+ 	print("A")            
+ 	if no>500:
+ 			print("D")
+ 	if no<+500:
+ 			print ("E") 		
+ 	print("1")            
