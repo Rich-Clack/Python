0fabe1068eab4092ffabe9806fadbe93855b2eef
Rich-Clack <rclacknbs@gmail.com>
2019-05-24 10:15:28 +0100

    added new python files

---
+++ Result.py
@@ -0,0 +1,6 @@
+Physics=80
+Chemistry=100
+Maths=100
+Total=Physics+Chemistry+Maths
+print("The Result is")
+print(Total)
---
+++ Result2.py
@@ -0,0 +1,12 @@
+Physics=80
+Chemistry=100
+Maths=100
+Total=Physics+Chemistry+Maths
+Per=Total*100/450
+print("Physics:", Physics)
+print ("Chemistry:", Chemistry)
+print("Maths:",Maths)
+print("_-_-_-_-_-_-_-_-_-_-_-_-_-")
+print("Total Marks:", Total)
+print("Percentage:", Per)
+print("-_-_-_-_-_-_-_-_-_-_-_-_-_")
