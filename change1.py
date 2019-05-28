ad4340f2ead68b28a1aeeabe6ee2aa8ac63105a8
Rich-Clack <rclacknbs@gmail.com>
2019-05-28 14:41:39 +0100

    Python print vertically & reverse added

--- change1.py
+++ change1.py
@@ -7,24 +7,22 @@ print("The following change is needed")
 if balance/50>=1:
 	print (int(balance/50), " x £50")
 	balance = balance%50
-	print(balance)
+	print("Remaining Balance :£",balance)
 if balance/20>=1:
 	print (int(balance/20), " x £20")
 	balance=balance%20
-	print(balance)
+	print("Remaining Balance :£",balance)
 if balance/10>=1:
 	print (int(balance/10), " x £10")
 	balance=balance%10
-	print(balance)
+print("Remaining Balance :£",balance)
 if balance/5>=1:
 	print (int(balance/5), " x £5")
 	balance=balance%5	
-	print(balance)
+print("Remaining Balance :£",balance)
 if balance/2>=1:
 	print (int(balance/2), " x £2")
 	balance=balance%2
-	print(balance)
+print("Remaining Balance :£",balance)
 if balance/1>=1:
 	print (int(balance/1), " x £1")
-	balance=balance%1	
-	print(balance)
